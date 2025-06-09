29cd4ac4ae4fd2be25ba1f9c117c8ff33abbf827
Muslim Omar <mislimumer@gmail.com>
2025-06-10 00:05:19 +0200

    docs: test build actions

--- auth/src/db/connectDataSource.ts
+++ auth/src/db/connectDataSource.ts
@@ -3,7 +3,7 @@ import myDataSource from './data-source';
 const connectDataSource = async () => {
   try {
     await myDataSource.initialize();
-    console.log('Data Source has been initialized!');
+    console.log('Data Source has been initialized!!');
   } catch (err) {
     console.error('Error during Data Source initialization:', err);
     throw err;
